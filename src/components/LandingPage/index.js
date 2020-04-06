0f0e2db82a9a222437f3cf52f94df30d00ab5c59
Madeuss <mateus1two3@gmail.com>
2020-04-06 01:24:10 -0300

    Load weather info

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 /* BOOTSTRAP COMPONENTS */
 import Form from 'react-bootstrap/Form'
@@ -15,18 +15,6 @@ export default function LandingPage() {
     const [id, setID] = useState('')
     const [weather, setWeather] = useState([])
 
-    {/*useEffect(() => {
-        api.get('weather', {
-            params: {
-                q: `${country}`,
-                appid: `${API_KEY}`
-            }
-        }
-        ).then(response => {
-           console.log(response.data);
-       })
-   }, [])*/}
-
    async function handleConsultWeather(e) {
         e.preventDefault();
 
@@ -38,6 +26,7 @@ export default function LandingPage() {
                 params: {
                     q: `${country_name}, ${country_id}`,
                     appid: `${API_KEY}`,
+                    units: 'metric'
                 }
             }).then(response => {
                 console.log(response.data)
@@ -45,7 +34,7 @@ export default function LandingPage() {
             })
         }
         catch (err) {
-            alert('Search Error! Check the city name')
+            alert('Search Error! Check the city name or the country ID')
         }
     }
 
@@ -53,30 +42,68 @@ return (
     <>
         <container className="weather-container">
             <section className="form-section">
-                <form className="inputs" onSubmit={handleConsultWeather}>
-                    <Form.Row>
-                        <Form.Label column="lg" lg={0}>
-                            City:
-                        </Form.Label>
-                        <Col className="input-col-1" >
-                            <Form.Control value={country} onChange={e => setCountry(e.target.value)} id="city-name" size="lg" type="text" placeholder="City Name" />
-                        </Col>
-                    </Form.Row>
-                    <Form.Row className="form-row">
-                        <Form.Label column="lg" lg={0}>
-                            Country ID:
-                        </Form.Label>
-                        <Col className="input-col-2" >
-                            <Form.Control value={id} onChange={e => setID(e.target.value)} id="country-id" size="lg" type="text" placeholder="ID" />
-                        </Col>
-                    </Form.Row>
-                    <Button type="submit" variant="dark">Consult</Button>
-                </form>
-                
-                <div className="weather-data">
-                    
-                </div>
+                <aside className="left"></aside>
+                <aside className="right">
+                    <form className="inputs" onSubmit={handleConsultWeather}>
+                        <Form.Row className="form-row">
+                            <Form.Label column="lg" lg={0}>
+                                City:
+                            </Form.Label>
+                            <Col id="input-col-1" className="input-col" >
+                                <input value={country} onChange={e => setCountry(e.target.value)} id="city-name" size="lg" type="text" placeholder="City Name" />
+                            </Col>
+                        </Form.Row>
+                        <Form.Row className="form-row">
+                            <Form.Label column="lg" lg={0}>
+                                Country ID:
+                            </Form.Label>
+                            <Col id="input-col-2" className="input-col" >
+                                <input value={id} onChange={e => setID(e.target.value)} id="country-id" size="lg" type="text" placeholder="ID" />
+                            </Col>
+                        </Form.Row>
+                        <Button type="submit" variant="dark">Consult</Button>
+                    </form>
 
+                    <div className="weather-data">
+
+                        {/* City - Country */}
+                        <div className="data-group city_country">
+                            <h2> {weather.name}</h2>
+                            { weather.sys? (
+                                <>
+                                    <h2> -{weather.sys.country}</h2>
+                                </>
+                                ) : (
+                                <div></div> 
+                            )} 
+                        </div>
+
+                        {/* Main info and Weather Description */}
+                        { weather.weather ? (
+                            weather.weather.map(w => 
+                            <>
+                                <h1>{w.main}</h1>
+                                <p>{w.description}</p>
+                            </>
+                            )) : (
+                            <div></div> 
+                        )}
+                        <br></br>
+
+                        {/* Temp and Humidity */}
+                        { weather.main ? (
+                            <>
+                                <h2>{weather.main.temp} ºC</h2>
+                                <div className="data-group">
+                                    <span>Humidity: </span>
+                                    <p>{weather.main.humidity}%</p>
+                                </div>
+                            </>
+                            ) : (
+                            <div></div> 
+                        )}  
+                    </div>
+                </aside>               
             </section>
         </container>
     </>
