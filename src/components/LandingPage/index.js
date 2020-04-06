61cd29aaa1b644804cb745b96b8d6fc98d626adf
Madeuss <mateus1two3@gmail.com>
2020-04-06 16:32:02 -0300

    Detalhes 'finais' e busca de responsividade

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 /* BOOTSTRAP COMPONENTS */
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
+import Carousel from 'react-bootstrap/Carousel'
+
+import bg1 from '../../assets/bg1.jpeg'
+import bg2 from '../../assets/bg2.jpeg'
+import bg3 from '../../assets/bg3.jpeg'
+import bg4 from '../../assets/bg4.jpeg'
+import bg5 from '../../assets/bg5.jpeg'
+import bg6 from '../../assets/bg6.jpeg'
+import bg7 from '../../assets/bg7.jpeg'
+import bg8 from '../../assets/bg8.jpeg'
+import bg9 from '../../assets/bg9.jpeg'
+import bg10 from '../../assets/bg10.jpeg'
+import bg11 from '../../assets/bg11.jpeg'
+import bg12 from '../../assets/bg12.jpeg'
+import bg13 from '../../assets/bg13.jpeg'
+import bg14 from '../../assets/bg14.jpeg'
 
 import './styles.css'
 
@@ -42,7 +57,52 @@ return (
     <>
         <container className="weather-container">
             <section className="form-section">
-                <aside className="left"></aside>
+                <aside className="left">
+                    <Carousel className="Carousel" interval={8000} controls={false} indicators={false}>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg1} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg3} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg5} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg7} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg9} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg11} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg13} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg2} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg4} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg6} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg8} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg10} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg12} alt="First slide"/>
+                        </Carousel.Item>
+                        <Carousel.Item className="carousel-item-1">
+                            <img className="d-block w-100" src={bg14} alt="First slide"/>
+                        </Carousel.Item>
+                    </Carousel>
+                </aside>
                 <aside className="right">
                     <form className="inputs" onSubmit={handleConsultWeather}>
                         <Form.Row className="form-row">
@@ -61,17 +121,26 @@ return (
                                 <input value={id} onChange={e => setID(e.target.value)} id="country-id" size="lg" type="text" placeholder="ID" />
                             </Col>
                         </Form.Row>
-                        <Button type="submit" variant="dark">Consult</Button>
+                        <button className="btn-consult" type="submit">Consult</button>
                     </form>
 
                     <div className="weather-data">
+                        {/* Weather Icon */}
+                        { weather.weather ? (
+                            weather.weather.map(w => 
+                            <>
+                                 <img src={`http://openweathermap.org/img/wn/${w.icon}@2x.png`} alt="weather-icon" />
+                            </>
+                            )) : (
+                            <div></div> 
+                        )}
 
                         {/* City - Country */}
                         <div className="data-group city_country">
                             <h2> {weather.name}</h2>
                             { weather.sys? (
                                 <>
-                                    <h2> -{weather.sys.country}</h2>
+                                    <h2>, {weather.sys.country}</h2>
                                 </>
                                 ) : (
                                 <div></div> 
--- src/services/api.js
+++ src/services/api.js
@@ -3,15 +3,5 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'https://api.openweathermap.org/data/2.5', 
 })
-
-{/*api.interceptors.request.use(config => {
-    config.params = {
-        q: 'London',
-        appid: 'ba6d400600da3da64a7f04693de0c28f',
-     // spread the request's params
-      ...config.params,
-    };
-    return config;
-  });*/}
   
 export default api;
